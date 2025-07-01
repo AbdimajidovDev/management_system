84adad1d1223f5429c1be545d70ed255d4c64e39
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2025-07-01 12:36:30 +0500

    add groups serializer

---
+++ app/groups/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from app.groups.views import GroupViewSet
+
+
+router = DefaultRouter()
+router.register('groups', GroupViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
