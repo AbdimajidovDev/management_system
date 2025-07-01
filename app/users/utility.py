918ec33d49c645fa33e230defca8b89b6241588a
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2025-07-01 12:38:17 +0500

    permission

--- app/users/utility.py
+++ app/users/utility.py
@@ -4,4 +4,31 @@ from app.users.models import User
 
 class IsSuperAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and  User.UserRoles.superadmin)# and request.user.is_authenticated)
+        user = request.user
+        print('-----------------------------')
+        print("Super User:", request.user)
+        print("Role:", getattr(request.user, "role", None))
+        print("is_superuser:", request.user.is_superuser)
+        print("is_authenticated:", request.user.is_authenticated)
+        print('-----------------------------')
+
+        return bool(user and user.is_authenticated and  user.role == User.UserRoles.superadmin)
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        print('-----------------------------')
+        print("User:", request.user)
+        print("Role:", getattr(request.user, "role", None))
+        print("is_superuser:", request.user.is_superuser)
+        print("is_authenticated:", request.user.is_authenticated)
+        print('-----------------------------')
+
+        return bool(user and user.is_authenticated and user.role == User.UserRoles.admin)
+
+
+class IsTeacher(permissions.BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        return bool(user and user.is_authenticated and user.role == User.UserRoles.teacher)
