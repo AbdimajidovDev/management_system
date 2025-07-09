7ac469e6af421444267e84dc969469545ad62058
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2025-07-09 17:53:41 +0500

    calculate_teacher_salary func

--- app/groups/views.py
+++ app/groups/views.py
@@ -10,3 +10,37 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+# ////////////////////////////////////////////////////////////////////////////
+
+
+from app.attendance.models import Attendance
+from app.groups.models import Group
+
+
+
+
+def calculate_teacher_salary(group: Group, month: int, year: int):
+    lesson_dates = Attendance.objects.filter(
+        group=group,
+        date__year=year,
+        date__month=month,
+    ).values('date').distinct()
+
+
+    lesson_count = lesson_dates.count()
+
+    if lesson_count == 0:
+        return 0
+
+    attendance_count = Attendance.objects.filter(
+        group=group,
+        date__year=year,
+        date__month=month,
+        status=['p', 'a']).count()
+
+    per_lesson_price = group.price / lesson_count
+    total_income = per_lesson_price * attendance_count
+    salary = total_income // 2
+
+    # return salary
+    return round(salary, 2)
