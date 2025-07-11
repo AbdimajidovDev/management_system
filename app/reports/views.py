876b0b9632c8b3526bf6475f5e86c8f6f2590ca5
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2025-07-11 15:22:55 +0500

    techer_salary_view

--- app/reports/views.py
+++ app/reports/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+
+from app.groups.models import Group
+from app.reports.utility import calculate_teacher_salary
+
+
+@extend_schema(tags=['teacher'])
+def teacher_salary_view(request):
+    teacher = request.user
+    groups = Group.objects.filter(teacher=teacher)
+
+    group_id = request.GET.get('group')
+    group = groups.filter(id=group_id).first() if group_id else groups.first()
+
+    month = int(request.GET.get('month', datetime.now().month))
+    year = int(request.GET.get('year', datetime.now().year))
+
+    salary = None
+    if group:
+        salary = calculate_teacher_salary(group, month, year)
+    return render(request, 'salaries/teacher_salary.html',{
+        'groups': groups,
+        'group': group,
+        'month': month,
+        'year': year,
+        'salary': salary})
