a3fb31f4627826aa87b44dfe98e6c433ee9b2385
To'lqinjon <tulqinjonabdimajidov@gmail.com>
2025-09-06 21:35:08 +0500

    fixed

--- app/attendance/admin.py
+++ app/attendance/admin.py
@@ -1,8 +1,12 @@
 from calendar import month_name
 from datetime import datetime, timedelta, date
-from django.contrib import admin as django_admin
-from django.shortcuts import render
 from unfold import admin as unfold_admin
+
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib import admin as django_admin
+from django.contrib import messages
+from django.urls import reverse
 from django import forms
 
 from app.attendance.models import Attendance
@@ -28,146 +32,6 @@ class AttendanceInline(unfold_admin.TabularInline):
     ordering = ['date']
 
 # # # -------------------------------------------------------------------------------------------->
-
-from datetime import date, timedelta, datetime
-from calendar import month_name
-from django.shortcuts import render
-from unfold.admin import ModelAdmin as UnfoldModelAdmin
-
-
-# @django_admin.register(Attendance)
-# class AttendanceAdmin(UnfoldModelAdmin):
-#     change_list_template = 'admin/attendance_matrix.html'
-#
-#     def changelist_view(self, request, extra_context=None):
-#         user = request.user
-#
-#         groups = self.get_user_groups(user)
-#         group = self.get_selected_group(request, groups)
-#         students = self.get_group_students(group)
-#
-#         current_date = date.today()
-#         selected_month = request.GET.get('month', current_date.strftime('%Y-%m'))
-#         selected_date = self.parse_selected_date(selected_month, current_date)
-#
-#         month_start, month_end = self.get_month_range(selected_date)
-#         lesson_weekdays = self.get_group_weekdays(group)
-#         lesson_dates = self.get_lesson_dates(month_start, month_end, lesson_weekdays)
-#
-#         months = self.get_available_months(group)
-#         attendance_data = self.get_attendance_data(students, group, lesson_dates)
-#
-#         extra_context = extra_context or {}
-#         extra_context.update({
-#             'students': students,
-#             'dates': lesson_dates,
-#             'attendance_data': attendance_data,
-#             'group': group,
-#             'groups': groups,
-#             'months': months,
-#             'selected_month': selected_month,
-#             'today': current_date.strftime('%Y-%m-%d'),
-#             'user': user,
-#         })
-#         return render(request, self.change_list_template, extra_context)
-#
-#     def get_user_groups(self, user):
-#         if user.role == User.UserRoles.teacher:
-#             return Group.objects.filter(teacher=user)
-#         elif user.role == User.UserRoles.parent:
-#             return Group.objects.filter(
-#                 id__in=StudentGroup.objects.filter(student__parent=user).values('group_id')
-#             ).distinct()
-#         return Group.objects.all()
-#
-#     def get_selected_group(self, request, groups):
-#         group_id = request.GET.get('group')
-#         if group_id:
-#             return groups.filter(id=group_id).first()
-#         return groups.first()
-#
-#     def get_group_students(self, group):
-#         if not group:
-#             return StudentGroup.objects.none()
-#         return StudentGroup.objects.filter(group=group)
-#
-#     def parse_selected_date(self, month_str, fallback_date):
-#         try:
-#             return datetime.strptime(month_str, '%Y-%m').date()
-#         except ValueError:
-#             return fallback_date
-#
-#     def get_month_range(self, selected_date):
-#         month_start = selected_date.replace(day=1)
-#         if selected_date.month == 12:
-#             month_end = selected_date.replace(year=selected_date.year + 1, month=1, day=1)
-#         else:
-#             month_end = selected_date.replace(month=selected_date.month + 1, day=1)
-#         return month_start, month_end
-#
-#     def get_group_weekdays(self, group):
-#         if not group:
-#             return list(range(6))  # all days by default
-#         if group.type == Group.GroupType.even:
-#             return [1, 3, 5]
-#         elif group.type == Group.GroupType.odd:
-#             return [0, 2, 4]
-#         return list(range(6))  # all days
-#
-#     def get_lesson_dates(self, start, end, weekdays):
-#         return [
-#             start + timedelta(days=i)
-#             for i in range((end - start).days)
-#             if (start + timedelta(days=i)).weekday() in weekdays
-#         ]
-#
-#     def get_available_months(self, group):
-#         if not group:
-#             return []
-#
-#         months = []
-#         current = group.start_date.replace(day=1)
-#         while current <= group.end_date:
-#             months.append({
-#                 'value': current.strftime('%Y-%m'),
-#                 'label': f"{month_name[current.month]} {current.year}"
-#             })
-#             if current.month == 12:
-#                 current = current.replace(year=current.year + 1, month=1)
-#             else:
-#                 current = current.replace(month=current.month + 1)
-#         return months
-#
-#     def get_attendance_data(self, students, group, dates):
-#         data = {}
-#         for student_rel in students:
-#             student_id = student_rel.student.id
-#             data[student_id] = {}
-#             for d in dates:
-#                 att = Attendance.objects.filter(
-#                     student=student_rel.student,
-#                     group=group,
-#                     date=d
-#                 ).first()
-#                 data[student_id][d.strftime('%Y-%m-%d')] = att.status if att else 'e'
-#         return data
-#
-#     def get_queryset(self, request):
-#         qs = super().get_queryset(request)
-#         user = request.user
-#         if user.role == User.UserRoles.teacher:
-#             return qs.filter(group__teacher=user)
-#         elif user.role == User.UserRoles.parent:
-#             return qs.none()  # Parent should not access this queryset directly
-#         return qs
-
-
-
-# # # -------------------------------------------------------------------------------------------->
-
-from django.contrib import messages
-from django.shortcuts import redirect
-from django.urls import reverse
 
 class AttendanceAdmin(unfold_admin.ModelAdmin):
     change_list_template = 'admin/attendance_matrix.html'
